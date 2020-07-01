9fca7bede5e4cb8196ee9d29c1307a237532fc54
jvmops <jvmops@gmail.com>
2020-07-01 15:57:41 +0200

    Initial commit

---
+++ src/test/java/com/jvmops/gradle/modules/GradleSnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvmops.gradle.modules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
